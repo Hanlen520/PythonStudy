250983baddf9654e93bac85803b41ee5cb006a2c
indeyo <indeyo@git.com>
2020-04-27 20:23:17 +0800

    issue-3:05 third solution of replace_space

--- leetcode/replace_space.py
+++ leetcode/replace_space.py
@@ -31,7 +31,7 @@ class ReplaceSpace:
         # 如果不copy，直接赋值，那么对array_new进行修改会影响到array
         array_new = array.copy()
         # 代表当前空格后面的字符串收尾指针
-        p_head, p_tail = 0, len(array)-1
+        p_head, p_tail = 0, len(array) - 1
         # 原字符串扫描指针
         p_search = 0
 
@@ -39,13 +39,13 @@ class ReplaceSpace:
             if i == ' ':
                 # 这种切片赋值法有一种情况不能实现，不能默认输入空字符，赋值后list后面都是有值的
                 # 当结尾是一个空格加一个字符的时候，最后一个字符会丢失
-                last = array[p_search+1:len(array)]
+                last = array[p_search + 1:len(array)]
                 if len(last) < 2:
                     array_new.append("")
-                    array_new[p_head+3:p_tail+3] = last
+                    array_new[p_head + 3:p_tail + 3] = last
                 else:
-                    array_new[p_head+3:p_tail+3] = array[p_search+1:len(array)]
-                array_new[p_head:p_head+3] = "%20"
+                    array_new[p_head + 3:p_tail + 3] = array[p_search + 1:len(array)]
+                array_new[p_head:p_head + 3] = "%20"
                 p_head += 3
                 p_tail += 3
                 p_search += 1
@@ -67,6 +67,34 @@ class SimpleReplaceSpace:
         return "".join(new)
 
 
+class LessSpaceReplaceSpace:
+    def replaceSpace(self, s):
+        array = list(s)
+        length = len(array)
+        p, blank_num = 0, 0
+
+        while p < length:
+            if array[p] == " ":
+                blank_num += 1
+                array.append('x')
+                array.append('x')
+            p += 1
+
+        p_orgin_tail = length - 1
+        p_new_tail = length - 1 + 2 * blank_num
+
+        while p_orgin_tail >= 0:
+            if array[p_orgin_tail] == " ":
+                # python不支持给空的list用切片赋值
+                array[p_new_tail-2:p_new_tail+1] = ["%", "2", "0"]
+                # array.insert(p_new_tail-2, "%20")
+                p_new_tail -= 3
+            else:
+                array[p_new_tail] = array[p_orgin_tail]
+                p_new_tail -= 1
+            p_orgin_tail -= 1
+        return "".join(array)
+
 if __name__ == '__main__':
     # a = [1, 2, 3, 4, 5, 6, 7, 8]
     # b = ['l', 'o', 'v', 'e']
@@ -87,6 +115,8 @@ if __name__ == '__main__':
     print(s.replaceSpace(" aeee e"))
     s1 = SimpleReplaceSpace()
     print(s1.replaceSpace(" aeee e"))
+    s2 = LessSpaceReplaceSpace()
+    print(s2.replaceSpace(" aeee e"))
 
     # a = []
     # q = queue.Queue()
--- leetcode/units/test_replace_space.py
+++ leetcode/units/test_replace_space.py
@@ -8,13 +8,14 @@
 """
 import pytest
 
-from leetcode.replace_space import ReplaceSpace, SimpleReplaceSpace
+from leetcode.replace_space import ReplaceSpace, SimpleReplaceSpace, LessSpaceReplaceSpace
 
 
 class TestReplaceSpace:
     def setup(self):
         self.s = ReplaceSpace()
         self.s1 = SimpleReplaceSpace()
+        self.s2 = LessSpaceReplaceSpace()
 
     @pytest.mark.parametrize("s", [
         "hello world",
@@ -27,6 +28,7 @@ class TestReplaceSpace:
     def test_success(self, s):
         print(self.s.replaceSpace(s))
         print(self.s1.replaceSpace(s))
+        print(self.s2.replaceSpace(s))
 
     @pytest.mark.parametrize("s", [
         " ",
@@ -37,3 +39,4 @@ class TestReplaceSpace:
     def test_fail(self, s):
         print(self.s.replaceSpace(s))
         print(self.s1.replaceSpace(s))
+        print(self.s2.replaceSpace(s))
