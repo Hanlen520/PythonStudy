c417f7bdee68ca886e58d050a345d8cb751c6c9e
indeyo <indeyo@git.com>
2020-04-28 21:49:01 +0800

    issue-5：13.roman_to_int

---
+++ leetcode/units/test_roman_to_int.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Project : PythonStudy
+@File    : test_roman_to_int.py
+@Time    : 2020-04-28  21:05:37
+@Author  : indeyo_lin
+"""
+import pytest
+
+from leetcode.roman_to_int import RomanToInt
+
+
+class TestRomanToInt:
+    def setup(self):
+        self.s = RomanToInt()
+
+    @pytest.mark.parametrize("s, int_num", [
+        ("I", 1),
+        ("V", 5),
+        ("VI", 6),
+        ("IV", 4),
+        ("III", 3),
+        ("IIXLC", 140),
+        ("MCMXCIV", 1994),
+        ("LVIII", 58)
+    ])
+    def test_success(self, s, int_num):
+        assert self.s.romanToInt(s) == int_num
