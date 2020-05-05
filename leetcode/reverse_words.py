8ddc8bd62a95de8234ca86cfa83bca30f5117ac5
indeyo <indeyo@git.com>
2020-05-05 15:34:56 +0800

    add a new solution of reverse_words

--- leetcode/reverse_words.py
+++ leetcode/reverse_words.py
@@ -30,20 +30,13 @@ class ReverseWords(object):
     输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
     如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
     """
-
-    # 注意：LeetCode上面的python版本比较老，不支持类型提示，记得去掉：str
     def reverse(self, s: str):
         """字符串内容全部翻转"""
-        # 空字符串不处理
-        if s == "":
-            return ""
-
         array = list(s)
         begin = 0
         end = len(s) - 1
-        temp = ""
 
-        while begin <= end:
+        while begin < end:
             temp = array[begin]
             array[begin] = array[end]
             array[end] = temp
@@ -53,32 +46,22 @@ class ReverseWords(object):
 
     # 提交的时候记得把函数名改成默认的那个，要不然LeetCode找不到函数会报错
     def reverse_words(self, s: str):
-        """
-        只翻转单词顺序，不翻转单词
-        :type s: str
-        :rtype: str
-        """
-        # 整个字符串翻转
-        array = self.reverse(s).split(" ")
-        # 把多余的空格去掉
-        # while "" in array:
-        #     array.remove("")
-        print(array)
-        point = 0
+        """只翻转单词顺序，不翻转单词"""
+        # 整个字符串翻转，并切割成数组
+        arr = self.reverse(s).split()
+        # print(arr)
         array_new = []
 
         # 每个单词翻转
-        for item in array:
-            if item == "":
-                # array发生改变，遍历算法有问题
-                # array.remove("")
-                continue
+        for item in arr:
             array_new.append(self.reverse(item))
-            # array_new[point] = self.reverse(item)
-            # point += 1
-        # print(array)
-        # return " ".join(array)
         return " ".join(array_new)
+
+    def reverse_words2(self, s: str):
+        """方法二：使用内置函数"""
+        strs = s.split()
+        strs.reverse()
+        return " ".join(strs)
 
 
 if __name__ == '__main__':
@@ -87,7 +70,7 @@ if __name__ == '__main__':
     # print(s.reverse_words("Today is a nice day."))
 
     array = ["1", "2", "3"]
-    array.extend(["9","7"])
+    array.extend(["9", "7"])
     # array.append([9,9,9])
     print(array)
     array.append("")
--- leetcode/units/test_reverse_words.py
+++ leetcode/units/test_reverse_words.py
@@ -15,13 +15,14 @@ class TestReverseWords:
     def setup(self):
         self.reverse = ReverseWords()
 
-    @pytest.mark.parametrize("str1", [
-        "the sky is blue",
-        "  hello world!  ",
-        "a good   example"
+    @pytest.mark.parametrize("str1,str2", [
+        ("the sky is blue", "blue is sky the"),
+        ("  hello world!  ", "world! hello"),
+        ("a good   example", "example good a")
     ])
-    def test_success(self, str1):
-        print(self.reverse.reverse_words(str1))
+    def test_success(self, str1, str2):
+        assert self.reverse.reverse_words(str1) == str2
+        assert self.reverse.reverse_words2(str1) == str2
 
     @pytest.mark.parametrize("str1", [
         " ",
@@ -29,3 +30,4 @@ class TestReverseWords:
     ])
     def test_fail(self, str1):
         print(self.reverse.reverse_words(str1))
+        print(self.reverse.reverse_words2(str1))
